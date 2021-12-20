14bfde3572d69c5a412c8b6b0d515b4466ff53ea
jadenK123 <41663450+jadenK123@users.noreply.github.com>
2021-12-20 13:37:51 +0900

    watch 설정

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,6 @@ var gulp = require('gulp'),
     sass = require('gulp-sass')(require('sass')),
     concatCss = require('gulp-concat-css'),
     watch = require('gulp-watch');
-    // cleanCSS = require('gulp-clean-css'),
-    // liveload = require('gulp-livereload');
-//var csscomb = require('gulp-csscomb');
 
 //경로변수
   src = './src',
@@ -35,19 +32,9 @@ gulp.task('concat', function () {
     .pipe(gulp.dest(dist));//임시 
 });
 
-//css minify
-// gulp.task('minify', () => {
-//   return gulp.src(src + '/css/*.css')
-//     .pipe(cleanCSS({compatibility: 'ie8'}))
-//     .pipe(gulp.dest(dist));
-// });
-
-gulp.task('sass:watch', function () {
-  gulp.watch(paths.scss, ['sass']);  
-  gulp.watch(src + '/scss/*.scss', ['sass', 'concat']); 
+gulp.task('watch', function () {
+  // gulp.watch(paths.scss, ['sass'])
+  gulp.watch(src + '/scss/*.scss', gulp.series(['sass', 'concat'])); 
 });
 
-// gulp.task('default', gulp.series('gulp_sass', 'concat'));
-
-// gulp.task('default', gulp.series(['sass', 'concat', 'minify']));
-// gulp.task('default', gulp.series(['sass', 'sass:watch', 'concat']));
+gulp.task('default', gulp.series(['sass', 'concat', 'watch']));
